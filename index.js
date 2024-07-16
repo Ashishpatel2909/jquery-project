5573229927c10f0b13ce646bae35b6950216c020
Ashish kumar patel <168217463+Ashishpatel2909@users.noreply.github.com>
2024-07-16 21:45:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+$("button.one").click(function() {
+    $("h1").css("color", "purple");
+    $("h1").text("ASHISH");
+})
+$("button.two").click(function() {
+    $("h1").css("color", "green");
+    $("h1").text("ANISH");
+})
+$("button.three").click(function() {
+    $("h1").css("color", "red");
+    $("h1").text("SANJU");
+})
+$("button.four").click(function() {
+    $("h1").css("color", "BLUE");
+    $("h1").text("AVANISH");
+})
+$("button.five").click(function() {
+    $("h1").css("color", "orange");
+    $("h1").text("AMIT");
+})
+
